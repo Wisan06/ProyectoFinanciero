178764fea2021fcb6999be018a5b3aa1bb7c2e84
Wisan06 <65031696+Wisan06@users.noreply.github.com>
2023-11-04 17:01:53 -0500

    Micro Transaccion.java

---
+++ Transaccion.java
@@ -0,0 +1,39 @@
+package com.transacciones.transaccion;
+public class Transaccion {
+    private String fecha;
+    private String descripcion;
+    private double monto;
+    private String dependencia;
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
+    public String getDependencia() {
+        return dependencia;
+    }
+
+    public void setDependencia(String dependencia) {
+        this.dependencia = dependencia;
+    }
+}
